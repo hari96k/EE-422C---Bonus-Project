91747c3dc2482f6edb72d07c116a3e4afa040fe7
Hari Krishna Kosuru <hari96k@utexas.edu>
2016-04-28 22:42:42 -0500

    Final touchups and cleanup

--- bonus/Driver.java
+++ bonus/Driver.java
@@ -7,8 +7,13 @@ package bonus;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		Game masterMind = new Game();
-		masterMind.runGame();
-	}
+    public static void main(String[] args) {
+        Game masterMind;
+
+        // Create a game object as long as the user requests to play again
+        do {
+            masterMind = new Game();
+        }
+        while (masterMind.runGame());
+    }
 }
